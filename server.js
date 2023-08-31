3181e32c5eb205528e09acc33d9bb251b4c8a432
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-08-31 20:08:21 +0200

    .env vars

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = require('./app');
 // Database connection
 mongoose
   .connect(
-    `mongodb+srv://sroczykarkadiusz:ZbH93PDeSLz0NdUD@cluster0.hlwuwiw.mongodb.net/db-contacts`
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.hlwuwiw.mongodb.net/db-contacts`
   )
   .then(() => console.log('Database connection successful!'))
   .catch(() => process.exit(1));
 
-const port = 3000;
+const port = process.env.PORT | 3000;
 app.listen(port, () => {
-  console.log('Server running. Use our API on port: 3000');
+  console.log(`Server running. Use our API on port: ${port}`);
 });
