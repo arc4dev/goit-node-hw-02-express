ead130977b6135e36ef30ba30481cdd0d40cec6f
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-09-22 18:01:29 +0200

    jest test prepare

--- __tests__/authController.test.js
+++ __tests__/authController.test.js
@@ -1,7 +1,28 @@
-const { login } = require('../controllers/authController.js');
+const request = require('supertest');
+const User = require('../models/userModel.js');
+const app = require('../app.js');
 
 describe('Auth API', function () {
-  test('test', () => {
-    expect(1 + 2).toBe(3);
+  let testUser = {};
+
+  beforeAll(async () => {
+    testUser = await User.create({
+      email: 'test@test.com',
+      password: 'test123',
+    });
   });
+  // Test case for successfull login
+  test('Properly log in user an return user data and token', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({ email: testUser.email, password: testUser.password });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+    expect(response.body.user.email).toBe(testUser.email);
+    expect(response.body.subscription).toBe(testUser.subscription);
+  });
+
+  // Test case for unsuccessfull login
+  // test('Not logging in user and return error message');
 });
